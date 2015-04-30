08844d18d65c7a841f7a145a89db14f8cf32dd83
drumichiro <portnoy1207@gmail.com>
2015-04-30 03:03:05 +0100

    Added a setting of initial phase.

--- synthesizer.py
+++ synthesizer.py
@@ -8,11 +8,19 @@ Created on 2015/04/23
 
 import numpy as np
 import warnings
+import enum  # enum34
+
+
+##############################
+Phase = enum.Enum('Phase', 'ZERO RANDOM SCHROEDER')
+
+##############################
 
 
 class Synthesizer:
     # Synthesizes a simple sine wave.
-    def __init__(self, samplingRate, bufferLength, frameLength):
+    def __init__(self, samplingRate, bufferLength, frameLength,
+                 initialPhase=Phase.RANDOM):
         if bufferLength < frameLength:
             raise Exception("Buffer length must be more than frame length.")
         if 0 < (bufferLength % frameLength):
@@ -30,23 +38,49 @@ class Synthesizer:
         # Preset default parameters.
         baseValue = np.ones((self.BUFFER_LENGTH,
                              self.DEFAULT_SINES), np.float)
-        amplitude = baseValue*self.DEFAULT_BASE_AMP/self.DEFAULT_SINES
+        self.amplitude = baseValue*self.DEFAULT_BASE_AMP/self.DEFAULT_SINES
+        # Peak trajectories to generate phase.
         peakFreq = np.arange(1, self.DEFAULT_SINES+1)*self.DEFAULT_F0_FREQUENCY
-        frequency = peakFreq*baseValue
+        self.frequency = peakFreq*baseValue
         # freqAmp = (1.0 + 0.2*np.sin(np.arange(0, self.BUFFER_LENGTH)/4000.0))
         # frequency[:, 0] = frequency[:, 0]*freqAmp
         # frequency[:, 1] = frequency[:, 1]*freqAmp
-        phase = self.calculateInstantPhase(frequency)
-        self.setParameter(amplitude, phase)
+        self.setInitialPhase(initialPhase)
 
-    def calculateInstantPhase(self, frequency):
+    def calculateInstantPhase(self, frequency, initialPhase):
         phaseBase = np.zeros(frequency.shape)
-        sumFreq = 0.0
+        sumFreq = initialPhase[:]
         for freq, phs in zip(frequency, phaseBase):
             sumFreq += freq
             phs[:] = sumFreq
         coef = 2*np.pi/self.SAMPLING_RATE
         return phaseBase*coef
+
+    def getSineNum(self):
+        return self.amplitude.shape[1]
+
+    def generateRandomPhase(self):
+        np.random.seed(0xabe)
+        return np.random.random(self.getSineNum())*self.SAMPLING_RATE
+
+    def generateSchroederPhase(self):
+        # TODO Implement!
+        return self.generateRandomPhase()
+
+    def generateZeroPhase(self):
+        return np.zeros(self.getSineNum())
+
+    def setInitialPhase(self, phaseType):
+        initialPhase = []
+        if phaseType == Phase.ZERO:
+            initialPhase = self.generateZeroPhase()
+        elif phaseType == Phase.RANDOM:
+            initialPhase = self.generateRandomPhase()
+        elif phaseType == Phase.SCHROEDER:
+            initialPhase = self.generateSchroederPhase()
+        else:
+            raise Exception("Invalid phase type is input.")
+        self.phase = self.calculateInstantPhase(self.frequency, initialPhase)
 
     def setParameter(self, amplitude=[], phase=[]):
         if len(amplitude):
