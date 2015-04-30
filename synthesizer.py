2c9c201039c9ecdb8f72b3eccec97d1259045ce5
drumichiro <portnoy1207@gmail.com>
2015-04-30 09:19:17 +0100

    Fixed a bug due to wrong position. Abolished some default parameters, goodbye. Changed a setting of default parameters.

--- synthesizer.py
+++ synthesizer.py
@@ -7,7 +7,6 @@ Created on 2015/04/23
 '''
 
 import numpy as np
-import warnings
 import enum  # enum34
 
 
@@ -19,33 +18,36 @@ Phase = enum.Enum('Phase', 'ZERO RANDOM SCHROEDER')
 
 class Synthesizer:
     # Synthesizes a simple sine wave.
-    def __init__(self, samplingRate, bufferLength, frameLength,
-                 initialPhase=Phase.RANDOM):
-        if bufferLength < frameLength:
-            raise Exception("Buffer length must be more than frame length.")
-        if 0 < (bufferLength % frameLength):
-            warnings.warn("Buffer length is not multiple of frame length.")
+    def __init__(self, samplingRate, amplitude=None, frequency=None,
+                 initialPhaseType=Phase.RANDOM):
 
         # Initialize parameters
         self.SAMPLING_RATE = samplingRate
-        self.BUFFER_LENGTH = bufferLength
-        self.DEFAULT_BASE_AMP = 0.1
-        self.DEFAULT_F0_FREQUENCY = samplingRate/8
-        self.DEFAULT_SINES = 2
-        self.frameLength = frameLength
+        self.initialPhaseType = initialPhaseType
         self.position = 0
 
         # Preset default parameters.
-        baseValue = np.ones((self.BUFFER_LENGTH,
-                             self.DEFAULT_SINES), np.float)
-        self.amplitude = baseValue*self.DEFAULT_BASE_AMP/self.DEFAULT_SINES
-        # Peak trajectories to generate phase.
-        peakFreq = np.arange(1, self.DEFAULT_SINES+1)*self.DEFAULT_F0_FREQUENCY
-        self.frequency = peakFreq*baseValue
-        # freqAmp = (1.0 + 0.2*np.sin(np.arange(0, self.BUFFER_LENGTH)/4000.0))
-        # frequency[:, 0] = frequency[:, 0]*freqAmp
-        # frequency[:, 1] = frequency[:, 1]*freqAmp
-        self.setInitialPhase(initialPhase)
+        amplitude, frequency = self.presetDefaultParameter(amplitude,
+                                                           frequency)
+
+        self.amplitude = np.array(amplitude)
+        self.frequency = np.array(frequency)
+        self.sines = None
+        if None is not frequency:
+            self.calculateSines()
+
+    def presetDefaultParameter(self, amplitude, frequency):
+        if None is amplitude and None is not frequency:
+            length = frequency.shape[0]
+            sinNum = frequency.shape[1]
+            amplitude = np.ones((length, sinNum), np.float)/sinNum
+        elif None is not amplitude and None is frequency:
+            length = amplitude.shape[0]
+            sinNum = amplitude.shape[1]
+            fundFreq = self.SAMPLING_RATE/2.0/length
+            peakFreq = np.arange(1, sinNum+1)*fundFreq
+            frequency = peakFreq*np.ones((length, sinNum), np.float)
+        return amplitude, frequency
 
     def calculateInstantPhase(self, frequency, initialPhase):
         phaseBase = np.zeros(frequency.shape)
@@ -56,8 +58,33 @@ class Synthesizer:
         coef = 2*np.pi/self.SAMPLING_RATE
         return phaseBase*coef
 
+    def calculateSines(self):
+        initialPhase = []
+        if self.initialPhaseType == Phase.ZERO:
+            initialPhase = self.generateZeroPhase()
+        elif self.initialPhaseType == Phase.RANDOM:
+            initialPhase = self.generateRandomPhase()
+        elif self.initialPhaseType == Phase.SCHROEDER:
+            initialPhase = self.generateSchroederPhase()
+        else:
+            raise Exception("Invalid phase type is input.")
+        self.phase = self.calculateInstantPhase(self.frequency, initialPhase)
+        self.sines = np.cos(self.phase)  # Use cosine function.
+
+    def getAmpLength(self):
+        if 0 >= len(self.amplitude.shape):
+            return 1
+        return self.amplitude.shape[0]
+
+    def getFreqLength(self):
+        if 0 >= len(self.frequency.shape):
+            return 1
+        return self.frequency.shape[0]
+
     def getSineNum(self):
-        return self.amplitude.shape[1]
+        if 1 >= len(self.frequency.shape):
+            return 1
+        return self.frequency.shape[1]
 
     def generateRandomPhase(self):
         np.random.seed(0xabe)
@@ -70,36 +97,38 @@ class Synthesizer:
     def generateZeroPhase(self):
         return np.zeros(self.getSineNum())
 
-    def setInitialPhase(self, phaseType):
-        initialPhase = []
-        if phaseType == Phase.ZERO:
-            initialPhase = self.generateZeroPhase()
-        elif phaseType == Phase.RANDOM:
-            initialPhase = self.generateRandomPhase()
-        elif phaseType == Phase.SCHROEDER:
-            initialPhase = self.generateSchroederPhase()
-        else:
-            raise Exception("Invalid phase type is input.")
-        self.phase = self.calculateInstantPhase(self.frequency, initialPhase)
+    def setAmplitude(self, amplitude):
+        self.amplitude = amplitude
 
-    def setParameter(self, amplitude=[], phase=[]):
-        if len(amplitude):
-            self.amplitude = amplitude
-        if len(phase):
-            self.phase = phase
-        if self.amplitude.shape != self.phase.shape:
-            raise Exception("Lengths are not different.")
+    def setFrequency(self, frequency):
+        self.frequency = frequency
+        self.calculateSines()
+        # Reset position
+        self.position = 0
+
+    def popCurrentSines(self):
+        begin = self.position
+        end = self.position + self.getAmpLength()
+        assert begin < self.getAmpLength()
+        assert end <= self.getAmpLength()
+        # Update position
+        self.position = end % self.getFreqLength()
+        if end <= self.getFreqLength():
+            # Return sines directly.
+            return self.sines[begin:end]
+        # Cross the boundary of sines.
+        sines = self.sines[begin:]
+        while True:
+            end -= self.getFreqLength()
+            if end <= self.getFreqLength():
+                break
+            sines = np.r_[sines, self.sines]
+        return np.r_[sines, self.sines[:end]]
 
     def getStream(self):
         # Generate sine wave.
-        begin = self.position
-        end = self.position + self.frameLength
-        sines = np.cos(self.phase[begin:end])*self.amplitude[begin:end]
-
-        # Update position
-        rest = self.BUFFER_LENGTH - end
-        self.position = end if rest >= self.frameLength else 0
-
+        # print "self.position:%d" % self.position
+        sines = self.popCurrentSines()*self.amplitude
         return np.sum(sines, axis=1)
 
     def getSamplingRate(self):
