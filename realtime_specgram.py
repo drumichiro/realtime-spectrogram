9cf67460ce6f5ff033314456ed42ca1e540f6f20
drumichiro <portnoy1207@gmail.com>
2015-05-02 18:01:23 +0100

    Draws power envelope. Draws spectrum envelope.

--- realtime_specgram.py
+++ realtime_specgram.py
@@ -33,7 +33,7 @@ BufferPopEvent = threading.Event()
 
 
 class AudioListener(threading.Thread):
-    def __init__(self):
+    def __init__(self, samplingRate, frameLength):
         super(AudioListener, self).__init__()
         self.SAMPLING_RATE = 44100
         self.FRAME_LENGTH = 1024
@@ -98,6 +98,8 @@ class SpecgramListener(threading.Thread):
         self.running = True
         bins = self.FRAME_LENGTH/2 + 1
         self.spectrogram = np.zeros((bins, frames), np.float)
+        self.spectrum = np.zeros(bins, np.float)
+        self.envelope = np.zeros(frames, np.float)
         self.window = np.hamming(self.FRAME_LENGTH)
         self.frames = frames
 
@@ -113,7 +115,9 @@ class SpecgramListener(threading.Thread):
 
             BufferSemaphore.acquire()   # Lock.
             signal = self.releaseSignalBuffer()
-            self.spectrogram[:, pos:pos+1] = self.transformToSpecgram(signal)
+            self.spectrum = self.transformToSpectrum(signal)
+            self.spectrogram[:, pos:pos+1] = self.spectrum.reshape((-1, 1))
+            self.envelope = np.sum(self.spectrogram, axis=0)
             BufferSemaphore.release()   # Unlock.
             pos = (pos + 1) % self.frames
             if not BufferPopEvent.is_set():
@@ -143,44 +147,69 @@ class SpecgramListener(threading.Thread):
         else:
             return np.append(SignalBuffer[begin:], SignalBuffer[:end])
 
-    def transformToSpecgram(self, signal):
+    def transformToSpectrum(self, signal):
         return specgram(signal, NFFT=self.FRAME_LENGTH,
-                        noverlap=0, window=self.window)[0].reshape((-1, 1))
+                        noverlap=0, window=self.window)[0]
 
     def getSpecgramReference(self):
         return self.spectrogram
 
+    def getSpectrumReference(self):
+        return self.spectrum
 
-def updatePlot(i1, spectrogram, axes, img):
-    img.set_data(spectrogram)
-    # TODO Implement plotting envelopes.
-    line, = axl.plot([i1 % 20, 0, 0])
-    return [line, img]
+    def getEnvelopeReference(self):
+        return self.envelope
+
+
+def updatePlot(i1, spec, axes, img, line):
+    axl = axes[1]
+    axb = axes[2]
+    # Ignore drawing first frame.
+    if 0 == i1:
+        return []
+
+    img.set_data(spec.getSpecgramReference())
+    if 1 < i1:
+        axl.lines.pop(0)
+        axb.lines.pop(0)
+    BufferSemaphore.acquire()
+    left, = axl.plot(spec.getSpectrumReference(), line, "b")
+    bottom, = axb.plot(spec.getEnvelopeReference(), "b")
+    BufferSemaphore.release()
+    return [left, bottom, img]
 
 
 if __name__ == '__main__':
+    samplingRate = 44100
+    frameLength = 1024
+    bins = frameLength/2 + 1
+    frames = 150
 
     fig = plt.figure(figsize=(20, 10))
     gs = gridspec.GridSpec(2, 2, width_ratios=[1, 5], height_ratios=[5, 1])
     axs = plt.subplot(gs[0, 1])
     axl = plt.subplot(gs[0, 0], sharey=axs)
     axb = plt.subplot(gs[1, 1], sharex=axs)
+    axs.set_xlim([0, frames])
+    axs.set_ylim([0, bins])
+    axl.set_xlim([0, 0.3])
+    axb.set_ylim([0, 3.0])
     axes = [axs, axl, axb]
+    line = np.linspace(0.0, bins - 1, bins)
 
-    audio = AudioListener()
-    spec = SpecgramListener(1024, 150)
-    spectrogram = spec.getSpecgramReference()
-    img = axs.matshow(spectrogram, aspect="auto", origin='lower',
-                      norm=LogNorm(vmin=1e-10, vmax=1))
+    audio = AudioListener(samplingRate, 1024)
+    spec = SpecgramListener(frameLength, frames)
+    img = axs.matshow(spec.getSpecgramReference(), aspect="auto",
+                      origin='lower', norm=LogNorm(vmin=1e-10, vmax=1))
     BufferPushEvent.set()
     BufferPopEvent.set()
 
     # Start plot.
     anime = animation.FuncAnimation(fig, updatePlot, interval=50, blit=True,
-                                    fargs=(spectrogram, axes, img))
+                                    fargs=(spec, axes, img, line))
     audio.start()
     spec.start()
-    print "Showing."
+    print "Drawing."
     plt.show()
     audio.stop()
     spec.stop()
